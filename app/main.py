36d32e42dc24f19c3d8a2b776253cc5a0a3ef24e
RAMOS <pedrohramos083@gmail.com>
2025-11-13 10:44:00 -0300

    add cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import notas, usuario, authenticate, nota_not_auth
 
@@ -7,6 +8,13 @@ app = FastAPI(
     description="Projeto FastAPI para a criação de rotas com link linkados a um QrCode e usando MongoDB Atlas com data-base",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(usuario.router)
 app.include_router(authenticate.router)
